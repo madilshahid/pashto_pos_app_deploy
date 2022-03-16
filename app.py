19b1136df0890232fcbb4bc31c4341655995cc93
madilshahid <89195552+madilshahid@users.noreply.github.com>
2022-03-16 14:23:02 +0500

    Update app.py

--- app.py
+++ app.py
@@ -11,9 +11,11 @@ with tf.device("cpu:0"):
 
 with open('data/data.pkl', 'rb') as f:
     X_train, Y_train, word2int, int2word, tag2int, int2tag = pickle.load(f)
-
+    pred_tag = Y_train
+    print(pred_tag)
     del X_train
     del Y_train
+
 
 # flask
 app = Flask(__name__)
@@ -33,26 +35,30 @@ def prediction():
     for word in sentence:
         tokenized_sentence.append(word2int[word])
 
-    tokenized_sentence = np.asarray([tokenized_sentence])
-    padded_tokenized_sentence = pad_sequences(tokenized_sentence, maxlen=50)
-    with tf.device("cpu:0"):
-        my_prediction = model.predict(padded_tokenized_sentence)
-
-
     dates = {}
-    int2tag[0] = 'Unknown'
-    check = True
-    y = 49
-    z = len(sentence) - 1
-    while check:
-        b = np.argmax(my_prediction[0][y])
+    i = 0
+    for token in tokenized_sentence:
+        dates[sentence[i]] =  int2tag[pred_tag[token][0]]
+        i = i + 1
+
+#with tf.device("cpu:0"):
+    #my_prediction = model.predict(padded_tokenized_sentence)
+
+
+
+    #int2tag[0] = 'Unknown'
+    #check = True
+    #y = 49
+    #z = len(sentence) - 1
+    #while check:
+       # b = np.argmax(my_prediction[0][y])
 
         #dates = {sentence[z] : int2tag[b]}
-        dates[sentence[z]] = int2tag[b]
-        y = y - 1
-        z = z - 1
-        if (z < 0):
-            check = False
+        #dates[sentence[z]] = int2tag[b]
+        #y = y - 1
+        #z = z - 1
+        #if (z < 0):
+         #   check = False
 
 
     return render_template('prediction.html', dates = dates , prediction_text = abc )
